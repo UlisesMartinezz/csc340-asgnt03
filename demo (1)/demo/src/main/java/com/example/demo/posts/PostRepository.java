66dee93fcb817c43ac2d42abcdd9877df4242879
UlisesMartinezz <uamartinezz@gmail.com>
2026-07-06 22:59:26 -0400

    PostRepository

--- demo (1)/demo/src/main/java/com/example/demo/posts/PostRepository.java
+++ demo (1)/demo/src/main/java/com/example/demo/posts/PostRepository.java
@@ -1,4 +1,14 @@
 package com.example.demo.posts;
-public class PostRepository {
-    
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Character, Long> {
+  List<Character> findByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(String titleKeyword, String contentKeyword);
+
+  List<Character> findByAuthorContainingIgnoreCase(String author);
+
 }
